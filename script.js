bf599410c2e19cc8a10df7d4186090c6b4aa7857
Pascal <pascalyuen@gmail.com>
2022-07-30 15:55:19 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function getComputerChoice() {
-  const objects = ["Rock", "Paper", "Scissors"]
-  let random = Math.floor(Math.random() * objects.length);
-  return objects[random];
+    // Radomly returns Rock, Paper, or Scissors
+    const objects = ["Rock", "Paper", "Scissors"]
+    let random = Math.floor(Math.random() * objects.length);
+    return objects[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+    // Converts playerSelection and computerSelection to lowercase
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+  
+    // Logic for all possible outcomes of the game
+    if (playerSelection === computerSelection) {
+        return "Draw!"
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You win! Rock beats scissors!"
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats rock!"
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissors beat paper!"
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats scissors!"
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats rock!"
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "You lose! Scissors beat paper!"
+    }
+  }
