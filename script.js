856fe2508a6340c1a86e7fce8a40116176ad730c
Pascal <pascalyuen@gmail.com>
2022-07-30 16:21:14 +0800

    Add game function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerWinsRound = false;
+let score = 0;
+
 function getComputerChoice() {
     // Radomly returns Rock, Paper, or Scissors
     const objects = ["Rock", "Paper", "Scissors"]
@@ -14,16 +17,47 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "Draw!"
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You win! Rock beats scissors!"
+        playerWinsRound = true;
+        return "You win! Rock beats scissors!";
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "You lose! Paper beats rock!"
+        playerWinsRound = false;
+        return "You lose! Paper beats rock!";
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You win! Scissors beat paper!"
+        playerWinsRound = true;
+        return "You win! Scissors beat paper!";
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return "You lose! Rock beats scissors!"
+        playerWinsRound = false;
+        return "You lose! Rock beats scissors!";
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "You win! Paper beats rock!"
+        playerWinsRound = true;
+        return "You win! Paper beats rock!";
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return "You lose! Scissors beat paper!"
+        playerWinsRound = false;
+        return "You lose! Scissors beat paper!";
     }
   }
+
+  function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Do you choose Rock, Paper, or Scissors?");
+        console.log(playRound(playerChoice, getComputerChoice()));
+
+        // Add 1 score if the player wins the round
+        if (playerWinsRound) {
+            score += 1;
+        } 
+    }
+
+    // If the player wins 3 or more rounds, he is the winner of the game
+    if (score >= 3) {
+       console.log("You Win!");
+    } else {
+        console.log("You lose!");
+    }
+
+    // Reset the variables to prepare for new game
+    playerWinsRound = false;
+    score = 0;
+  }
+
+  game();
